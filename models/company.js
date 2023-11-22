29af71184c567af710df3a1a488797ca510240dc
Gema Kusuma <gemaakbarkusuma@gmail.com>
2023-11-22 18:26:06 +0700

    add validation model

--- models/company.js
+++ models/company.js
@@ -1,28 +1,69 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Company extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Company extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  Company.init({
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    image: DataTypes.STRING,
-    riskLevel: DataTypes.STRING,
-    growth: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Company',
-  });
-  return Company;
+
+    Company.init({
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please enter your Name'
+                }
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please enter your Description'
+                }
+            }
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please enter your Image'
+                }
+            }
+        },
+        riskLevel: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please enter your Risk Level'
+                }
+            }
+        },
+        growth: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please enter your Growth'
+                }
+            }
+        },
+    }, {
+        sequelize,
+        modelName: 'Company',
+    });
+    return Company;
 };
 
