412b9d49d9fbdf26e9b2a6fb38fce5da1baa56a0
Gema Kusuma <gemaakbarkusuma@gmail.com>
2023-11-22 18:12:01 +0700

    companies migration seeding

---
+++ models/company.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Company extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Company.init({
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    image: DataTypes.STRING,
+    riskLevel: DataTypes.STRING,
+    growth: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Company',
+  });
+  return Company;
+};
+
---
+++ seeders/20231122102253-seed-companies.js
@@ -0,0 +1,35 @@
+'use strict';
+const fs = require('fs').promises;
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+
+        let companies = JSON.parse(await fs.readFile("./data/companies.json", "utf-8")).map(item => {
+            item.createdAt = new Date();
+            item.updatedAt = new Date();
+            return item;
+        });
+
+        await queryInterface.bulkInsert('Companies', companies);
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('Companies', null, {});
+    }
+};
